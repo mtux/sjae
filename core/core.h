6e1c5d8e9acc6b711b82090409bd291589c8b5ff
sje397 <sje397@200c22c1-0253-0410-a7cc-2773c7d12e9d>
2009-02-05 15:18:16 +0000

    update version added 'rounded corners' option to main window

--- core/core.h
+++ core/core.h
@@ -37,7 +37,7 @@ public:
 	Q_INVOKABLE QString encrypt(const QString &source, const QString &key = "");
 	Q_INVOKABLE QString decrypt(const QString &source, const QString &key = "");
 
-	Q_INVOKABLE QString version() {return "Alpha 6";}
+	Q_INVOKABLE QString version() {return "Alpha 7";}
 
 public:
 	void show_help();
